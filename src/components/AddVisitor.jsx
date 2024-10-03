42b2b15af8837d8ab2099008ed1d8010f28bf796
nabeelsha123 <166122962+nabeelsha123@users.noreply.github.com>
2024-10-03 20:03:28 +0530

    added visitors list

---
+++ src/components/AddVisitor.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const AddVisitor = () => {
+  return (
+    <div>
+      <div className="container">
+        <Navbar/>
+        <div className="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div className="row g-3">
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Firstname</label>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Lastname</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Purpose</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Whom To Meet</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <button className="btn btn-success">submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AddVisitor
---
+++ src/components/ViewAllVisitor.jsx
@@ -0,0 +1,55 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import Navbar from './Navbar'
+
+const ViewAllVisitor = () => {
+    const [data, changeData] = useState([])
+    const fetchDataFromApi = ()=>{
+        axios.get("http://35.170.103.9:4057/getvistors").then(
+            (response)=>{
+                changeData(response.data)
+            }
+        ).catch()
+    }
+    useEffect(()=>{fetchDataFromApi()},{})
+    return (
+        <div>
+            <Navbar />
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xl-12">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Firstname</th>
+                                    <th scope="col">Lastname</th>
+                                    <th scope="col">Purpose</th>
+                                    <th scope="col">Whome to meet</th>
+                                    <th scope="col">Date</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    data.map(
+                                        (value,index)=>{
+                                            return <tr>
+                                            <th scope="row">{value.firstname}</th>
+                                            <td>{value.lastname}</td>
+                                            <td>{value.purpose}</td>
+                                            <td>{value.whomToMeet}</td>
+                                            <td>{value.date}</td>
+                                        </tr>
+
+                                        }
+                                    )
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ViewAllVisitor
